6e98c6ede2d7899280ea520328f6b8acf790e957
SudheerPulipati <mailtopsudheer@gmail.com>
2014-10-15 23:27:34 +0530

    Added PurchaseOrderController.java

--- src/main/java/com/dkl/merchantdb/controller/HomeController.java
+++ src/main/java/com/dkl/merchantdb/controller/HomeController.java
@@ -1,27 +1,14 @@
 package com.dkl.merchantdb.controller;
 
 import java.text.DateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.dkl.merchantdb.bo.CompanyBO;
-import com.dkl.merchantdb.bo.PurchaseOrderBO;
-import com.dkl.merchantdb.to.CityGroupJSON;
-import com.dkl.merchantdb.to.CityGroupTO;
-import com.dkl.merchantdb.to.CompanyJSON;
-import com.dkl.merchantdb.to.CompanyTO;
-import com.dkl.merchantdb.to.PurchaseOrderTOS;
-import com.google.gson.Gson;
 
 /**
  * Handles requests for the application home page.
@@ -31,12 +18,6 @@ public class HomeController {
 
 	private static final Logger logger = LoggerFactory
 			.getLogger(HomeController.class);
-
-	@Autowired
-	private CompanyBO companyBO;
-	
-	@Autowired
-	private PurchaseOrderBO purchaseOrderBO;
 
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -55,61 +36,5 @@ public class HomeController {
 
 		return "home";
 	}
-
-	@RequestMapping(value = "/createCompany")
-	public String createCompany(Locale locale, Model model) {
-
-		// model.addAttribute("serverTime", formattedDate );
-		System.out.println("createCompany");
-		return "createCompany";
-	}
-
-	@RequestMapping(value = "/saveCompany")
-	public String saveCompany(CompanyTO companyTO, Model model) {
-		System.out.println(companyTO);
-		System.out.println(companyTO.getCity());
-		System.out.println(companyTO.getCompanyName());
-		int rowCount = companyBO.createCompany(companyTO);
-		if (rowCount > 0) {
-			model.addAttribute("companyCreationStatus",
-					"Company " + companyTO.getCompanyName()
-							+ "has created successfully.");
-		}
-		return "createCompany";
-	}
-
-	@RequestMapping(value = "/viewCompany")
-	public String viewCompany() {
-		return "viewCompany";
-	}
-
-	@RequestMapping(value = "/viewCompanyJSON")
-	@ResponseBody
-	public String viewCompanyJSON(Model model) {
-		List<CompanyTO> companyTOs = companyBO.viewCompany();
-		CompanyJSON companyJSON = new CompanyJSON();
-		companyJSON.setDraw(1);
-		companyJSON.setRecordsFiltered(57);
-		companyJSON.setRecordsTotal(57);
-		companyJSON.setData(companyTOs);
-		System.out.println(companyTOs.get(0).getCreationDate());
-		System.out.println(companyTOs.get(0).getModifiedDate());
-		System.out.println(companyTOs.get(0).getSlipsIndicator());
-		Gson gson = new Gson();
-		return gson.toJson(companyJSON);
-	}
 	
-	@RequestMapping(value = "/purchaseOrder")
-	public String purchaseOrder() {
-		return "purchaseOrder";
-	}
-	
-	@RequestMapping(value = "/savePurchaseOrder")
-	public String savePurchaseOrder(PurchaseOrderTOS purchaseOrderTO,Model model) {
-		System.out.println("purchaseOrder");
-		if(purchaseOrderBO.savePurchases(purchaseOrderTO).length>0){
-			model.addAttribute("success", "PurchaseOrder saved successfully.");
-		}
-		return "purchaseOrder";
-	}
 }
